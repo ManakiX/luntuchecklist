9ed0234e491953550bd52ec5eaca2e88c90cb35e
unknown <sogeyanaluntu@gmail.com>
2023-04-18 11:21:17 +0200

    fixed emty array problem

--- data/code.js
+++ data/code.js
@@ -1,11 +1,12 @@
 const form = document.getElementById('form');
 const taskInput = document.getElementById('task');
 const list = document.getElementById('list');
-
+let taskList = JSON.parse(localStorage.getItem('taskList')) !== null ? JSON.parse(localStorage.getItem('taskList')) : [];
 // Load tasks from localStorage on page load
 document.addEventListener('DOMContentLoaded', () => {
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  tasks.forEach(task => addTask(task));
+  console.log(JSON.parse(localStorage.getItem('taskList')))
+  const tasks = JSON.parse(localStorage.getItem('taskList')) || [];
+  renderList(taskList);
   console.log(tasks);
 });
 
@@ -14,38 +15,57 @@ document.addEventListener('DOMContentLoaded', () => {
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (taskInput.value.trim()) {
-    addTask(taskInput.value.trim());
-    saveTasksToLocalStorage();
+  
+    // saveTasksToLocalStorage();
+    console.log(taskList)
+    console.log(taskInput.value)
+    taskList.push(taskInput.value);
+    localStorage.setItem('taskList', JSON.stringify(taskList))
+    console.log(taskList)
     taskInput.value = '';
-    // location.reload()
+    location.reload()
   }
-  console.log(tasks);
 
 });
 
-function addTask(task) {
-  const li = document.createElement('li');
-  li.innerHTML = `
-    <span>${task}</span>
-    <button class="delete">Delete</button>
-  `;
-  list.appendChild(li);
+// function addTask(task) {
+//   const li = document.createElement('li');
+//   li.innerHTML = `
+//     <span>${task}</span>
+//     <button id='${task}' class="delete">Delete</button>
+//   `;
+//   list.appendChild(li);
+// }
+function renderList(taskList){
+  taskList.forEach((item, index) => {
+    const li = document.createElement('li');
+      li.innerHTML = `
+        <span>${item}</span>
+        <button id='${index}' class="delete">Delete</button>
+      `;
+      list.appendChild(li);
+  })
 }
 
 list.addEventListener('click', (e) => {
   if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
-    saveTasksToLocalStorage();
+    console.log(e.target.id)
+    // e.target.parentElement.remove();
+    taskList.splice(e.target.id, 1)
+    console.log(taskList)
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+location.reload()
+    // saveTasksToLocalStorage();
+    // taskList.splice()
     // location.reload()
-    AnimationEffect(TransitionEvent)
+    // AnimationEffect(TransitionEvent)
   }
-  console.log(tasks);
+  // console.log(tasks);
 
 });
 
-function saveTasksToLocalStorage() {
-    const tasks = Array.from(list.children).map(li => li.firstChild.textContent.trim());
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
-  
+// function saveTasksToLocalStorage() {
+//     const tasks = Array.from(list.children).map(li => li.firstChild.textContent.trim());
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+//   }
 
